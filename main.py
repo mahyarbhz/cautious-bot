edf007e88dc47712436c410ada4ed6e95d9f00d0
MahyarNV <behzadimahyar85@gmail.com>
2021-03-04 22:45:56 +0330

    Initial Project

---
+++ main.py
@@ -0,0 +1,90 @@
+import discord
+from discord.ext import commands
+from asyncio import *
+from time import *
+
+
+class CONFIG:
+    TOKEN = ''
+    PREFIX = ']'
+
+
+client = commands.Bot(command_prefix=CONFIG.PREFIX)
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print('Bot Onlined')
+
+
+@client.command(aliases=['help', 'helper', 'helping'])
+async def command_help(infox):
+    help_embed = discord.Embed(
+        colour=0x0A75AD,
+        title="Help ❓",
+        description="This is Help"
+    )
+    help_embed.set_footer(text='Hope you used this helps')
+    help_embed.set_author(name="MahyarNV", url='http://test.mbehzadi.ir')
+    await infox.send(embed=help_embed)
+
+
+@client.command(aliases=['GG', 'Gg', 'gg', 'gG'])
+async def command_gg(infox, *, pos='person'):
+    await infox.send(">>> " + infox.author.mention + ' you are a GG ' + pos + ' ;)')
+
+
+@client.command(aliases=['status', 'setstatus', 'setst', 'set_status'])
+async def command_status(infox, status_type):
+    if infox.message.author.guild_permissions.administrator:
+
+        if status_type == 'idle':
+            await client.change_presence(status=discord.Status.idle)
+            print('taghire status be idle')
+
+        elif status_type == 'dnd':
+            await client.change_presence(status=discord.Status.dnd)
+
+        elif status_type == 'online':
+            await client.change_presence(status=discord.Status.online)
+
+        else:
+            await infox.send(">>> " + infox.author.mention + ' Status e morede nazar vojud nadarad!')
+
+    else:
+        await infox.send(">>> " + infox.author.mention + " you don't have permission to do that❗❌")
+
+
+@client.command(aliases=['activity', 'act', 'active'])
+async def command_activity(infox, act_type='', *, act_text='...'):
+    if infox.message.author.guild_permissions.administrator:
+
+        if act_type == 'playing':
+            await client.change_presence(activity=discord.Game(name=act_text))
+
+        elif act_type == 'watching':
+            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=act_text))
+
+        elif act_type == 'listening':
+            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=act_text))
+
+        else:
+            await infox.send(">>> " + infox.author.mention + ' Status e morede nazar vojud nadarad!')
+
+    else:
+        await infox.send(">>> " + infox.author.mention + " you don't have permission to do that❗❌")
+
+
+@client.command(aliases=['clear'])
+async def command_clear(infox, clear_count=0):
+    if clear_count == 0:
+        await infox.send("0 message nemitavanad pak shavad❌")
+    else:
+        await infox.channel.purge(limit=int(clear_count) + 1)
+        await infox.send(">>> " + str(clear_count) + " message pak shod✔")
+        sleep(2)
+        await infox.channel.purge(limit=1)
+
+
+client.run(CONFIG.TOKEN)
