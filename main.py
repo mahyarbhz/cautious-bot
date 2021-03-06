95cbf1e62496e1e1ad2b6b439a90f4d6407afb81
MahyarNV <behzadimahyar85@gmail.com>
2021-03-06 14:26:24 +0330

    add Ranking using database

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from mysql.connector import connection
+
+cnx = connection.MySQLConnection(user='root', password='pass',
+                                 host='127.0.0.1',
+                                 database='bot')
+cursor = cnx.cursor()
+cursor.execute(("SELECT * FROM users"))
+rows = cursor.fetchall()
+print('Total Row(s):', cursor.rowcount)
+cursor.close()
+
 import discord
 from discord.ext import commands
 from asyncio import *
@@ -22,7 +33,8 @@ all_commands = ['help', 'helps', 'helper', 'helping',
                 'GG', 'Gg', 'gg', 'gG',
                 'status', 'setstatus', 'setst', 'set_status',
                 'activity', 'act', 'active',
-                'clear', 'cl']
+                'clear', 'cl',
+                'rank', 'score', 'level']
 
 
 @client.event
@@ -34,7 +46,45 @@ async def on_message(infox):
         else:
             await client.process_commands(infox)
     else:
-        pass
+        cursor = cnx.cursor()
+        cursor.execute(("SELECT * FROM users WHERE username LIKE " + str(infox.author.id)))
+        rows = cursor.fetchall()
+        cursor.close()
+        j = 0
+        for row in rows:
+            if int(infox.author.id) == int(row[1]):
+                if int(infox.guild.id) == int(row[3]):
+                    new_score = int(row[2]) + 5
+                    cursor = cnx.cursor()
+                    cursor.execute(
+                        "UPDATE users SET score=%s WHERE idusers='%d'" % (
+                            new_score, row[0]))
+                    cnx.commit()
+                    cursor.close()
+                    if new_score % 100 == 0:
+                        rank = new_score // 100
+                        await infox.channel.send(
+                            ">>> " + infox.author.mention + " shoma be levele " + str(rank) + " residid, mobarake :)) ")
+                elif j < len(rows):
+                    continue
+                else:
+                    cursor = cnx.cursor()
+                    cursor.execute(
+                        "INSERT INTO users (username, score, guild) VALUES (%d, %d, %d) " % (
+                            infox.author.id, 0, infox.guild.id))
+                    cnx.commit()
+                    cursor.close()
+
+            elif j < len(rows)-1:
+                continue
+            else:
+                cursor = cnx.cursor()
+                cursor.execute(
+                    "INSERT INTO users (username, score, guild) VALUES (%d, %d, %d) " % (
+                        infox.author.id, 0, infox.guild.id))
+                cnx.commit()
+                cursor.close()
+            j += 1
 
 
 @client.command(aliases=['help', 'helps', 'helper', 'helping'])
@@ -42,9 +92,11 @@ async def command_help(infox):
     help_embed = discord.Embed(
         colour=0x0A75AD,
         title="Help ❓",
-        description="Dastoorate Admin ha:\n```]status [status]```\n```]activity [activity(playing, watching, "
-                    "listening)]```\n```]clear [meghdar]``` "
-    )
+        description="Dastoorate Admin ha:" \
+                    "```]status [status]```" \
+                    "```]activity [activity] [matn]```" \
+                    "```]clear [meghdar]```" \
+        )
     help_embed.set_footer(text='Hope you used this helps')
     help_embed.set_author(name="MahyarNV", url='http://test.mbehzadi.ir')
     await infox.send(embed=help_embed)
@@ -78,19 +130,25 @@ async def command_status(infox, status_type):
 
 @client.command(aliases=['activity', 'act', 'active'])
 async def command_activity(infox, act_type='', *, act_text='...'):
+    print(infox)
     if infox.message.author.guild_permissions.administrator:
+        if act_text != '...':
+            if act_type == 'playing':
+                await client.change_presence(activity=discord.Game(name=act_text))
 
-        if act_type == 'playing':
-            await client.change_presence(activity=discord.Game(name=act_text))
+            elif act_type == 'watching':
+                await client.change_presence(
+                    activity=discord.Activity(type=discord.ActivityType.watching, name=act_text))
 
-        elif act_type == 'watching':
-            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=act_text))
+            elif act_type == 'listening':
+                await client.change_presence(
+                    activity=discord.Activity(type=discord.ActivityType.listening, name=act_text))
 
-        elif act_type == 'listening':
-            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=act_text))
+            else:
+                await infox.send(">>> " + infox.author.mention + ' Status e morede nazar vojud nadarad!')
 
         else:
-            await infox.send(">>> " + infox.author.mention + ' Status e morede nazar vojud nadarad!')
+            await infox.send(">>> " + infox.author.mention + " status bayad matn dashte bashad")
 
     else:
         await infox.send(">>> " + infox.author.mention + " you don't have permission to do that❗❌")
@@ -100,11 +158,26 @@ async def command_activity(infox, act_type='', *, act_text='...'):
 async def command_clear(infox, clear_count=0):
     if clear_count == 0:
         await infox.send("0 message nemitavanad pak shavad❌")
+
     else:
         await infox.channel.purge(limit=int(clear_count) + 1)
         await infox.send(">>> " + str(clear_count) + " message pak shod✔")
         # sleep(2)
         # await infox.channel.purge(limit=1)
 
-# End Incomplete branch
+
+@client.command(aliases=['rank', 'score', 'level'])
+async def command_rank(infox):
+    cursor = cnx.cursor()
+    cursor.execute(("SELECT * FROM users WHERE username LIKE " + str(infox.author.id)))
+    rows = cursor.fetchall()
+    cursor.close()
+    for row in rows:
+        if int(infox.author.id) == int(row[1]):
+            if int(infox.guild.id) == int(row[3]):
+                score = int(row[2])
+                await infox.send(">>> " + infox.author.mention + " ranke shoma " + str(score // 100) + " ast!")
+            else:
+                continue
+
 client.run(CONFIG.TOKEN)
